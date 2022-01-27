4dd257b73bf31e0de02271870a436b72db981875
VandelayUtd <drewhadley.guitar@gmail.com>
2022-01-27 11:29:23 -0500

    added filter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import { CardList } from './components/card-list/card-list';
 
 class App extends Component {
 
@@ -7,7 +8,8 @@ class App extends Component {
     super()
 
     this.state = {
-      monsters: []
+      monsters: [],
+      searchField: ""
     }
   }
 
@@ -17,14 +19,16 @@ class App extends Component {
     .then(users => this.setState({ monsters: users}))
   }
 
+
+
   render() {  
+    const { monsters, searchField } = this.state;
+    const filteredMonsters = monsters.filter(monster => 
+      monster.name.toLowerCase().includes(searchField.toLowerCase()))
     return (
       <div className="App">
-        {
-          this.state.monsters.map((monster) => ( 
-            <h1 key={monster.name} > {monster.name} </h1>
-          ))
-        }
+        <input type='search' value={this.state.searchField} placeholder='search monsters' onChange={ e => this.setState({searchField: e.target.value})}/>
+        <CardList monsters={ filteredMonsters } />
       </div>
     );
   }
